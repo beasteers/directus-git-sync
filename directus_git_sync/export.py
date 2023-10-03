06d2611523822021ef801e30cdcf87d7aef51cfc
Bea Steers <bea.steers@gmail.com>
2023-10-03 11:40:23 -0400

    set default url

--- directus_git_sync/export.py
+++ directus_git_sync/export.py
@@ -1,12 +1,12 @@
 import os
 import logging
-from . import EXPORT_DIR
+from . import EXPORT_DIR, URL
 from .api import API
 from .util import yaml_dump, str_dump
 log = logging.getLogger(__name__)
 
 
-def export(email, password, url, out_dir=EXPORT_DIR):
+def export(email, password, url=URL, out_dir=EXPORT_DIR):
     '''Dump the configuration of a Directus to disk (to be committed to git).'''
     assert url and email and password, "missing url and credentials"
     log.info(f"Exporting Directus schema and flows from {url}")
