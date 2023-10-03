6d7b865ea4b2f84813a6f63f14c9f10889cb2fa0
Bea Steers <bea.steers@gmail.com>
2023-10-03 11:42:25 -0400

    fix default precidence for export dir

--- directus_git_sync/__init__.py
+++ directus_git_sync/__init__.py
@@ -5,14 +5,15 @@ log.setLevel(getattr(logging, os.getenv("LOG_LEVEL") or "INFO"))
 
 URL = os.getenv("DIRECTUS_URL") or "http://localhost:8055"
 
-EXPORT_DIR = 'export'
+EXPORT_DIR = os.getenv("DIRECTUS_OUT_DIR")
 REPO = os.getenv("GITSYNC_REPO")
 LINK = os.getenv("GITSYNC_LINK")
 ROOT = os.getenv("GITSYNC_ROOT")
 if not LINK and REPO:
     LINK = REPO.split('/')[-1].removesuffix('.git')
-if ROOT and LINK:
+if not EXPORT_DIR and ROOT and LINK:
     EXPORT_DIR = os.path.join(ROOT or '/git', LINK)
+EXPORT_DIR = EXPORT_DIR or 'export'
 
 from .api import API
 from . import util
