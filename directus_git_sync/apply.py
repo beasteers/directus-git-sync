6f5c657452efcedacae69e659aed480ac221c3d6
Bea Steers <bea.steers@gmail.com>
2024-01-29 01:05:58 -0500

    cleanup

--- directus_git_sync/apply.py
+++ directus_git_sync/apply.py
@@ -2,7 +2,7 @@ import os
 import glob
 import logging
 from . import EXPORT_DIR, URL, EMAIL, PASSWORD
-from .util import yaml_load
+from .util import load_dir
 from .api import API
 log = logging.getLogger(__name__)
 
@@ -16,38 +16,20 @@ def apply(email=EMAIL, password=PASSWORD, url=URL, src_dir=EXPORT_DIR, only=None
     api = API(url)
     api.login(email, password)
 
-    # log.info("# --------------------------------- Settings --------------------------------- #")
-    # api.apply_settings(load_dir(f'{src_dir}/settings.yaml'))
+    api.apply_settings(load_dir(f'{src_dir}/settings.yaml'))
     if not only or 'schema' in only:
-        log.info("# ---------------------------------- Schema ---------------------------------- #")
-        schema = load_dir(f'{src_dir}/schema.yaml')
-        if schema:
-            diff = api.diff_schema(schema, force=force)
-            if diff:
-                log.info("Applying diff:")
-                log.info("Diff Applied: %s", api.apply_schema(diff['data']) or '')
-            else:
-                log.info("Schema up to date! %s", diff)
+        api.diff_apply_schema(load_dir(f'{src_dir}/schema.yaml'), force=force)
     if not only or 'flows' in only:
-        log.info("# ----------------------------------- Flows ---------------------------------- #")
         api.apply_flows(load_dir(f'{src_dir}/flows'))
         api.apply_operations(load_dir(f'{src_dir}/operations'))
     if not only or 'dashboards' in only:
-        log.info("# -------------------------------- Dashboards -------------------------------- #")
         api.apply_dashboards(load_dir(f'{src_dir}/dashboards'))
         api.apply_panels(load_dir(f'{src_dir}/panels'))
     if not only or 'webhooks' in only:
-        log.info("# --------------------------------- Webhooks --------------------------------- #")
         api.apply_webhooks(load_dir(f'{src_dir}/webhooks'))
     if not only or 'roles' in only:
-        log.info("# ----------------------------------- Roles ---------------------------------- #")
         api.apply_roles(load_dir(f'{src_dir}/roles'))
         api.apply_permissions(load_dir(f'{src_dir}/permissions'))
-
-def load_dir(src_dir):
-    if os.path.isfile(src_dir):
-        return yaml_load(src_dir)
-    return [yaml_load(f) for f in glob.glob(f'{src_dir}/*')]
 
 
 if __name__ == '__main__':
--- directus_git_sync/util.py
+++ directus_git_sync/util.py
@@ -3,60 +3,109 @@
 import os
 import re
 import csv
+import glob
 import json
 import yaml
-try:
-    from yaml import CLoader as Loader, CDumper as Dumper
-except ImportError:
-    from yaml import Loader, Dumper
+# try:
+#     from yaml import CLoader as Loader, CDumper as Dumper
+# except ImportError:
+#     from yaml import Loader, Dumper
 
 import logging
-log = logging.getLogger(__name__)
+log = logging.getLogger(__name__.split('.')[0])
+
+
+
+def export_one(data, out_dir, name, ext='yaml'):
+    state = _export_one(data, out_dir, name, ext)
+    log.info("%-11s :: %s.", name.title(), status_text(state))
+    return 
+
+def _export_one(data, out_dir, name, ext='yaml'):
+    existing = glob.glob(get_fname(out_dir, name, ext))  # TODO: search for other exts
+    if existing:
+        assert len(existing) == 1
+        existing_data = load_data(existing[0])
+        if existing_data == data:
+            return 'unchanged'
+        state = 'modified'
+    else:
+        state = 'new'
+    write_data(data, get_fname(out_dir, name, ext))
+    return state
+
+
+def export_dir(data, out_dir, name, keys=['name', 'id'], ext='yaml'):
+    counts = {'unchanged': 0, 'modified': 0, 'new': 0, 'deleted': 0}
+
+    out_dir_i = os.path.join(out_dir, name)
+    existing = {os.path.splitext(f)[0] for f in os.listdir(out_dir_i)} if os.path.exists(out_dir_i) else set()
+    current = set()
+    for i, d in enumerate(data):
+        name_i = '-'.join(f'{d.get(k)}' for k in keys) if keys else f'{i}'
+        state = _export_one(d, f'{out_dir}/{name}', name_i, ext)
+        counts[state] += 1
+        current.add(name_i)
+    
+    for name_i in existing - current:
+        log.warning("%s :: Removing %s", name, name_i)
+        # os.remove(get_fname(out_dir, name, ext))
+        counts['deleted'] += 1
+
+    if not any(counts.values()):
+        log.info("%-11s :: 🫥  none.", name.title())
+    else:
+        log.info(
+            "%-11s :: %s. %s. %s. %s.", 
+            name.title(), *(
+                status_text(k, i=counts[k]) 
+                for k in ['new', 'modified', 'deleted', 'unchanged']
+            ),
+        )
 
 
 
 def load_dir(src_dir):
     if os.path.isfile(src_dir):
-        return yaml_load(src_dir)
-    return [yaml_load(f) for f in glob.glob(f'{src_dir}/*')]
+        return load_data(src_dir)
+    return [load_data(f) for f in glob.glob(f'{src_dir}/*')]
 
-def dump_each(data, out_dir, name, keys=['name', 'id']):
-    for i, d in enumerate(data):
-        yaml_dump(d, f'{out_dir}/{name}', '-'.join(f'{d.get(k)}' for k in keys) if keys else f'{i}')
+# def load_dir(src_dir):
+#     if os.path.isfile(src_dir):
+#         return yaml_load(src_dir)
+#     return [yaml_load(f) for f in glob.glob(f'{src_dir}/*')]
+
+# def dump_each(data, out_dir, name, keys=['name', 'id']):
+#     for i, d in enumerate(data):
+#         name_i = '-'.join(f'{d.get(k)}' for k in keys) if keys else f'{i}'
+
+#         yaml_dump(d, f'{out_dir}/{name}', name_i)
 
 
 
-def yaml_load(path):
-    # log.debug(f"📖 Reading {path}")
-    return yaml.load(open(path), Loader=Loader)
+# def yaml_load(path):
+#     # log.debug(f"📖 Reading {path}")
+#     return yaml.load(open(path), Loader=Loader)
 
-def yaml_dump(data, out_dir, fname):
-    '''Write out YAML in a git diff-friendly format'''
-    return str_dump(yaml.dump(data), out_dir, fname, ext='.yaml', type_name=type(data).__name__)
+# def yaml_dump(data, out_dir, fname):
+#     '''Write out YAML in a git diff-friendly format'''
+#     return str_dump(yaml.dump(data), out_dir, fname, ext='.yaml', type_name=type(data).__name__)
 
-def str_dump(data, out_dir, fname, ext='', type_name=None):
-    path = os.path.join(out_dir, f'{clean(fname)}{ext}')
-    os.makedirs(os.path.dirname(path) or '.', exist_ok=True)
-    with open(path, 'w') as f:
-        f.write(data)
-    log.info(f"💾↓ Wrote {type_name or ''} to {path}")
-    return fname
+# def str_dump(data, out_dir, fname, ext='', type_name=None):
+#     path = get_fname(out_dir, fname, ext)
+#     os.makedirs(os.path.dirname(path) or '.', exist_ok=True)
+#     with open(path, 'w') as f:
+#         f.write(data)
+#     log.info(f"💾↓ Wrote {type_name or ''} to {path}")
+#     return fname
 
+
+def get_fname(out_dir, fname, ext):
+    return os.path.join(out_dir, f'{clean(fname)}.{ext.lstrip(".")}')
 
 def clean(fname):
     return re.sub(r'\s*[^-\s_A-Za-z0-9.]+\s*', ' ', fname)
 
-
-# class CSVWriter:
-#     def __init__(self, fh):
-#         self.fh = fh
-#         self.writer = None
-
-#     def write(self, row):
-#         if not self.writer:
-#             self.writer = csv.DictWriter(self.fh, fieldnames=list(row))
-#             self.writer.writeheader()
-#         self.writer.writerow(row)
 
 
 def dump_data(data, file_path):
@@ -71,9 +120,13 @@ def dump_data(data, file_path):
     - None
     """
     file_extension = file_path.split('.')[-1].lower()
+    # log.info(f"💾↓ pretend write to {file_path}")
+    # return
 
+    os.makedirs(os.path.dirname(file_path) or '.', exist_ok=True)
     if file_extension == 'csv':
         with open(file_path, 'w', newline='') as csv_file:
+            # iterator so you can write large csv files
             data = iter(data)
             first = next(data, None)
             if first is not None:
@@ -129,3 +182,39 @@ def load_data(file_path):
             return f.read()
     else:
         raise ValueError("Unsupported file format. Supported formats: csv, json, yaml/yml")
+
+
+
+def dict_diff(d1, d2):
+    missing1 = d2.keys() - d1
+    missing2 = d1.keys() - d2
+    mismatch = {k for k in d1.keys() & d2 if d1[k] != d2[k]}
+    return missing1, missing2, mismatch
+
+
+
+class C:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+COLORS = {'new': C.CYAN, 'modified': C.YELLOW, 'deleted': C.RED, 'unchanged': C.GREEN, 'none': C.BLUE}
+ICONS = {'new': '🌱', 'modified': '🔧', 'deleted': '🗑 ', 'unchanged': '🌲', 'none': '🫥'}
+
+
+def color_text(color, x, i=True):
+    return f'{color}{x}{C.END}'
+
+
+def status_text(status, fmt=None, i=True):
+    color = COLORS.get(status, status)
+    icon = ICONS.get(status,"")
+    fmt = fmt or status
+    fmt = f'{icon} {i} {fmt}' if i is not True else f'{icon} {fmt}'
+    return f'{color}{fmt}{C.END}' if i else fmt
